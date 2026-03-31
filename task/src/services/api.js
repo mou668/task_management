45d1c4cd5715f8151dbc158080f10c5922b7c619
Mounika <23eg105e50@anurag.edu.in>
2026-03-31 15:52:21 +0530

    internceptorcrted

--- task/src/services/api.js
+++ task/src/services/api.js
@@ -11,7 +11,12 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token')
-    if (token) config.headers['Authorization'] = `Bearer ${token}`
+
+    // ❌ DON'T send token for auth APIs
+    if (token && !config.url.includes('auth')) {
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+
     return config
   },
   (error) => Promise.reject(error)
